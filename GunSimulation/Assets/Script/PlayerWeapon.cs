d4232f32369b6195ccec4c9d70e241607204b625
wonjun17234 <2022256@sungil-i.kr>
2024-01-07 15:34:54 +0900

    Init9

--- GunSimulation/Assets/Script/PlayerWeapon.cs
+++ GunSimulation/Assets/Script/PlayerWeapon.cs
@@ -21,6 +21,8 @@ public class PlayerWeapon : MonoBehaviour
     public float bulletLifeTime = 5;
 
     public float cartridgeCaseLifeTime = 3;
+
+    private bool isShooting = false;
     void Start()
     {
         
@@ -29,9 +31,11 @@ public class PlayerWeapon : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetMouseButtonDown(0))
+        if(Input.GetMouseButtonDown(0) && !isShooting)
         {
+            isShooting = true;
             Fire();
+            temp();
         }
     }
 
@@ -43,7 +47,7 @@ public class PlayerWeapon : MonoBehaviour
             Physics.IgnoreCollision(bullet.GetComponent<Collider>(), bulletSpawn.parent.GetComponent<Collider>()); //부모 객체와 충돌 X
 
             bullet.transform.position = bulletSpawn.position; //스폰 지점으로 객체 이동
-            Vector3 rotation = bullet.transform.rotation.eulerAngles; //
+            Vector3 rotation = bullet.transform.rotation.eulerAngles; 
 
             bullet.transform.rotation = Quaternion.Euler(rotation.x, transform.eulerAngles.y, rotation.z);
 
@@ -51,6 +55,11 @@ public class PlayerWeapon : MonoBehaviour
 
             StartCoroutine(DestroyBulletAfterTime(bullet, bulletLifeTime));
         }
+
+    }
+
+    private void temp()
+    {
         {
             GameObject cartridgeCase = Instantiate(casingPrefab);
 
@@ -65,10 +74,10 @@ public class PlayerWeapon : MonoBehaviour
 
             StartCoroutine(DestroyBulletAfterTime(cartridgeCase, cartridgeCaseLifeTime));
         }
-
-        
-        
+        isShooting = false;
     }
+
+    
 
     private IEnumerator DestroyBulletAfterTime(GameObject bullet, float delay)
     {
