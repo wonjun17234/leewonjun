8150c469fea6ec459943cf9c98a350a36a9159fa
wonjun17234 <2022256@sungil-i.kr>
2024-01-14 00:32:37 +0900

    inOut

--- GunSimulation/Assets/Script/GunTestController.cs
+++ GunSimulation/Assets/Script/GunTestController.cs
@@ -2,12 +2,19 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UIElements;
 
 public class GunTestController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float RotationSpeed;
+    public float MoveSpeed;
     private int sign = 1;
     private bool isClicked = false;
+
+    private Vector2 PriPos;
+
+    private Vector2 currentPos;
     public void ButtonDown(int num)
     {
         sign *= num;
@@ -24,14 +31,33 @@ public class GunTestController : MonoBehaviour
     {
         
     }
-
+    
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         if(isClicked)
-        { 
-            transform.position = new Vector3 (Mathf.Clamp(transform.position.x - Time.deltaTime * sign, 0.5f, 2.0f) , 0, 0);
+        {
+            if (Vector3.Distance(Vector3.zero, transform.position) > 0.9f && Vector3.Distance(Vector3.zero, transform.position) < 2.0f) //사이 값이면 마음대로 움직이게
+            {
+                transform.position = transform.position + transform.forward * sign * MoveSpeed;
+            }
+            else if (Vector3.Distance(Vector3.zero, transform.position) <= 0.9f && sign < 0) //너무 앞이면 뒤로만 갈 수 있게
+            {
+                transform.position = transform.position + transform.forward * sign * MoveSpeed;
+            }
+            else if (Vector3.Distance(Vector3.zero, transform.position) >= 2.0f && sign > 0) //너무 뒤면 앞으로만 갈 수 있게
+            {
+                transform.position = transform.position + transform.forward * sign * MoveSpeed;
+            }            
         }
         
+        if (Input.GetMouseButton(1))
+        {
+            //공전 구현
+        
+        }
+        
+
+
     }
 }
