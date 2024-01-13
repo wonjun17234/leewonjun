ff7fe1af972dc108ccc25ed30d6249350f971711
wonjun17234 <2022256@sungil-i.kr>
2024-01-13 12:34:42 +0900

    trigger

--- GunSimulation/Assets/Script/PlayerWeapon.cs
+++ GunSimulation/Assets/Script/PlayerWeapon.cs
@@ -7,30 +7,32 @@ using UnityEngine;
 
 public class PlayerWeapon : MonoBehaviour
 {
-    public GameObject bulletPrefab;
-
-    public GameObject casingPrefab;
-
-    public GameObject slide;
-
+    public GameObject OBJBulletPrefab;
     public Transform bulletSpawn;
 
+    public GameObject OBJCasingPrefab;
     public Transform cartridgeCaseSpawn;
 
+    public GameObject OBJSlide;
+
+    public GameObject OBJTrigger;
+
+    public ParticleSystem particleObject;
+
     public float bulletSpeed = 30;
-
-    public float cartridgeCaseSpeed = 30;
-
     public float bulletLifeTime = 5;
 
+    public float cartridgeCaseSpeed = 30;
     public float cartridgeCaseLifeTime = 3;
+    
     private Vector3 startPos;
     private Vector3 targetPos;
+    
     private bool isShooting = false;
     void Start()
     {
-        startPos = slide.transform.localPosition;
-        targetPos = new Vector3(startPos.x, startPos.y, startPos.z - 0.05f);
+        startPos = OBJSlide.transform.localPosition;
+        targetPos = new Vector3(startPos.x, startPos.y, startPos.z - 0.045f);
     }
 
     // Update is called once per frame
@@ -40,14 +42,14 @@ public class PlayerWeapon : MonoBehaviour
         if (Input.GetMouseButtonDown(0) && !isShooting)
         {
             isShooting = true;
-            Fire();
+            StartCoroutine(shot());
         }
     }
-
+    
     private void Fire()
     {
         {
-            GameObject bullet = Instantiate(bulletPrefab);
+            GameObject bullet = Instantiate(OBJBulletPrefab);
 
             Physics.IgnoreCollision(bullet.GetComponent<Collider>(), bulletSpawn.parent.GetComponent<Collider>()); //�θ� ��ü�� �浹 X
 
@@ -60,32 +62,15 @@ public class PlayerWeapon : MonoBehaviour
 
             StartCoroutine(DestroyBulletAfterTime(bullet, bulletLifeTime));
             StartCoroutine(slideBack());
+            //이펙트 구현
+            particleObject.Play();
         }
     }
-    private IEnumerator slideBack()
-    {
-        float time = 0f;
-        while(time < 0.05f)
-        {
-            time += Time.deltaTime;
-            slide.transform.localPosition = Vector3.Lerp(startPos, targetPos, time * 20f);
-            yield return null;
-        }
-        time = 0;
-        sparking();
-        while (time < 0.05f)
-        {
-            time += Time.deltaTime;
-            slide.transform.localPosition = Vector3.Lerp(targetPos, startPos, time * 20f);
-            yield return null;
-        }
-        
-        isShooting = false;
-    }
+
     private void sparking()
     {
         {
-            GameObject cartridgeCase = Instantiate(casingPrefab);
+            GameObject cartridgeCase = Instantiate(OBJCasingPrefab);
 
             Physics.IgnoreCollision(cartridgeCase.GetComponent<Collider>(), cartridgeCaseSpawn.parent.GetComponent<Collider>());
 
@@ -99,6 +84,48 @@ public class PlayerWeapon : MonoBehaviour
             StartCoroutine(DestroyBulletAfterTime(cartridgeCase, cartridgeCaseLifeTime));
         }
     }
+    private IEnumerator shot()
+    {
+        float time = 0f;
+        float currentX;
+        while (time < 0.05f)
+        {
+            time += Time.deltaTime;
+            currentX = Mathf.Lerp(-90f, -60f, time * 20);
+            OBJTrigger.transform.localRotation = Quaternion.Euler(currentX, 0f, 0f);
+            yield return null;
+        }
+        Fire();
+        time = 0;
+        while (time < 0.05f)
+        {
+            time += Time.deltaTime;
+            currentX = Mathf.Lerp(-60f, -90f, time * 20);
+            OBJTrigger.transform.localRotation = Quaternion.Euler(currentX, 0f, 0f);
+            yield return null;
+        }
+    }
+    private IEnumerator slideBack()
+    {
+        float time = 0f;
+        while(time < 0.05f)
+        {
+            time += Time.deltaTime;
+            OBJSlide.transform.localPosition = Vector3.Lerp(startPos, targetPos, time * 20f);
+            yield return null;
+        }
+        time = 0;
+        sparking();
+        while (time < 0.05f)
+        {
+            time += Time.deltaTime;
+            OBJSlide.transform.localPosition = Vector3.Lerp(targetPos, startPos, time * 20f);
+            yield return null;
+        }
+        
+        isShooting = false;
+    }
+    
 
 
     private IEnumerator DestroyBulletAfterTime(GameObject bullet, float delay)
