966b922dfebd507be17fc9654d6fae8db9d52d66
wonjun17234 <2022256@sungil-i.kr>
2024-01-14 10:46:38 +0900

    GunTestScript

--- GunSimulation/Assets/Script/GunTestController.cs
+++ GunSimulation/Assets/Script/GunTestController.cs
@@ -7,14 +7,23 @@ using UnityEngine.UIElements;
 
 public class GunTestController : MonoBehaviour
 {
+    public float Yaxis;
+    public float Xaxis;
+
     public float RotationSpeed;
     public float MoveSpeed;
-    private int sign = 1;
+    public float MaxDistance;
+    public float MinDistance;
+    public float currentDistance;
+    private int sign = 1; //+버튼인지 -버튼인지
+
     private bool isClicked = false;
 
-    private Vector2 PriPos;
+    private Vector3 targetRotation;
+    private Vector3 currentVel;
 
-    private Vector2 currentPos;
+
+    private Vector3 mousePos_prev;
     public void ButtonDown(int num)
     {
         sign *= num;
@@ -29,35 +38,58 @@ public class GunTestController : MonoBehaviour
 
     void Start()
     {
+        //mousePos_prev = Input.mousePosition;
+        currentDistance = Vector3.Distance(Vector3.zero, transform.position);
+        targetRotation = new Vector3(0, Yaxis, 0);
         
     }
-    
+
     // Update is called once per frame
-    void FixedUpdate()
+    void LateUpdate()
     {
         if(isClicked)
         {
-            if (Vector3.Distance(Vector3.zero, transform.position) > 0.9f && Vector3.Distance(Vector3.zero, transform.position) < 2.0f) //사이 값이면 마음대로 움직이게
+            currentDistance = Vector3.Distance(Vector3.zero, transform.position);
+            if (currentDistance > MinDistance && currentDistance < MaxDistance) //사이 값이면 마음대로 움직이게
             {
                 transform.position = transform.position + transform.forward * sign * MoveSpeed;
             }
-            else if (Vector3.Distance(Vector3.zero, transform.position) <= 0.9f && sign < 0) //너무 앞이면 뒤로만 갈 수 있게
+            else if (currentDistance <= MinDistance && sign < 0) //너무 앞이면 뒤로만 갈 수 있게
             {
                 transform.position = transform.position + transform.forward * sign * MoveSpeed;
             }
-            else if (Vector3.Distance(Vector3.zero, transform.position) >= 2.0f && sign > 0) //너무 뒤면 앞으로만 갈 수 있게
+            else if (currentDistance >= MaxDistance && sign > 0) //너무 뒤면 앞으로만 갈 수 있게
             {
                 transform.position = transform.position + transform.forward * sign * MoveSpeed;
             }            
         }
-        
+   
         if (Input.GetMouseButton(1))
         {
-            //공전 구현
-        
+
+            Vector3 mouseDelta = Input.mousePosition - mousePos_prev;
+            Debug.Log(mouseDelta);
+            Yaxis = Yaxis + mouseDelta.x * RotationSpeed;
+            Xaxis = Xaxis - mouseDelta.y * RotationSpeed;
+            Xaxis = ClampAngle(Xaxis, -90f, 90f);
+            targetRotation = Vector3.SmoothDamp(targetRotation, new Vector3(Xaxis, Yaxis), ref currentVel, 0.12f);
+            this.transform.eulerAngles = targetRotation;
+
+            transform.position = Vector3.zero - transform.forward * currentDistance;
+
+
+            mousePos_prev = Input.mousePosition;
+        }
+        else
+        {
+            mousePos_prev = Input.mousePosition;
         }
         
 
 
+    }
+    private float ClampAngle(float angle, float min, float max)
+    {
+        return Mathf.Clamp(angle, min, max);
     }
 }
